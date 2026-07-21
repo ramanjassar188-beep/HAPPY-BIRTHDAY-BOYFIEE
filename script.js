061fb55ce4d8b81ae21ebf8a14a9417f5f3b6e28
ramanjassar188-beep <ramanjassar188@gmail.com>
2026-07-21 21:36:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,103 @@
 function openGift() {
 
-    document.getElementById("intro").classList.add("hidden");
+    document
+        .getElementById("intro")
+        .classList
+        .add("hidden");
 
-    document.getElementById("menu").classList.remove("hidden");
+
+    document
+        .getElementById("menu")
+        .classList
+        .remove("hidden");
 
 }
+
 
 
 function showSection(section) {
 
-    document.getElementById("menu").classList.add("hidden");
 
-    document.querySelectorAll(".content-section")
-        .forEach(element => {
-            element.classList.add("hidden");
-        });
+    document
+        .getElementById("menu")
+        .classList
+        .add("hidden");
 
-    document.getElementById(section)
-        .classList.remove("hidden");
+
+    document
+        .querySelectorAll(
+            ".content-section"
+        )
+        .forEach(
+            element => {
+
+                element
+                    .classList
+                    .add("hidden");
+
+            }
+        );
+
+
+    document
+        .getElementById(section)
+        .classList
+        .remove("hidden");
 
 }
 
 
+
 function goBack() {
 
-    document.querySelectorAll(".content-section")
-        .forEach(element => {
-            element.classList.add("hidden");
-        });
 
-    document.getElementById("menu")
-        .classList.remove("hidden");
+    document
+        .querySelectorAll(
+            ".content-section"
+        )
+        .forEach(
+            element => {
+
+                element
+                    .classList
+                    .add("hidden");
+
+            }
+        );
+
+
+    document
+        .getElementById("menu")
+        .classList
+        .remove("hidden");
+
+}
+
+
+
+function bloomBouquet() {
+
+
+    document
+        .getElementById("flowers")
+        .classList
+        .remove("hidden");
+
+}
+
+
+
+function playSong() {
+
+
+    const message =
+        document
+            .getElementById(
+                "song-message"
+            );
+
+
+    message.innerHTML =
+        "🎵 Our song is waiting for us... 🤍";
 
 }
